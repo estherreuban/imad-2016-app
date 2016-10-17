fced435c33e52f2550c18a06c414c759eaf15f3b
estherreuban <estherreuban@gmail.com>
2016-10-17 19:43:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 
-app.get('one', function (req, res) {
+app.get('/one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'one.html'));
 });
 
