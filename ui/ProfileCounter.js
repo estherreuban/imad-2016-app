db4f5fa95d23e900463395d3f3ad1c460b1738ae
estherreuban <estherreuban@gmail.com>
2016-10-19 22:19:59 +0530

    [imad-console] Updates ui/ProfileCounter.js

--- ui/ProfileCounter.js
+++ ui/ProfileCounter.js
@@ -14,16 +14,14 @@ var but = document.getElementById("button1");
 but.onclick = function(){
     //////////////Create a request
     var req = new XMLHttpRequest();
-    alert("1");
+    
     req.onreadystatechange = function (){
         if (req.readyState === XMLHttpRequest.DONE ){
-            alert("2");
+    
             if (req.status === 200){
-                alert("3");
                 var counter = req.responseText;
                 var span1=document.getElementById("count1");
                 span1.innerHTML=counter.toString();
-                alert(counter.toString());
             }
         }
     };
