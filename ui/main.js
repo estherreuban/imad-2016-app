a0818f87b3e5e98a4394588d094ba2b9138366bf
estherreuban <estherreuban@gmail.com>
2016-10-19 14:00:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,7 +7,7 @@ var img=document.getElementById('img1');
 var marginLeft=0;
 moveRight = function(){
     marginLeft+=3;
-    if (marginLeft > 400)
+    if (marginLeft > 800)
         marginLeft=0;
     img.style.marginLeft=marginLeft +'px';
 };
