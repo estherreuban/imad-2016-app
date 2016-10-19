8197c9f5932bcc03076849a14ac05202a117a971
estherreuban <estherreuban@gmail.com>
2016-10-19 14:19:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,16 +5,16 @@ element.innerHTML="fffffffff";);*/
 
 var img=document.getElementById('img1');
 var marginLeft=0;
-var marginTop=0;
+//var marginTop=0;
 moveRight = function(){
     marginLeft++;
-    marginTop++;
+  //  marginTop++;
     if (marginLeft > 1000)
         marginLeft=0;
-    if (marginTop>500)
-    marginTop=0;
+/*    if (marginTop>500)
+    marginTop=0;*/
     img.style.marginLeft=marginLeft +'px';
-    img.style.marginTop=marginTop +'px';
+   // img.style.marginTop=marginTop +'px';
 };
 
 img.onclick = function (){
