1c86481f5408d49d507c14d403e730eb8976c0bb
estherreuban <estherreuban@gmail.com>
2016-10-19 22:16:42 +0530

    [imad-console] Updates ui/ProfileCounter.js

--- ui/ProfileCounter.js
+++ ui/ProfileCounter.js
@@ -14,15 +14,35 @@ var but = document.getElementById("button1");
 but.onclick = function(){
     //////////////Create a request
     var req = new XMLHttpRequest();
+    alert("1");
     req.onreadystatechange = function (){
         if (req.readystate === XMLHttpRequest.DONE ){
+            alert("2");
             if (req.status === 200){
+                alert("3");
                 var counter = req.responseText;
                 var span1=document.getElementById("count1");
                 span1.innerHTML=counter.toString();
+                alert(counter.toString());
             }
         }
     };
     req.open('GET', 'http://estherreuban.imad.hasura-app.io/counter', true);
     req.send(null);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
