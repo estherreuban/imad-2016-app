41821d470622e6f1c31ef9c9ebb36252213a1817
estherreuban <estherreuban@gmail.com>
2016-10-20 18:49:44 +0530

    [imad-console] Updates ui/ProfileCounter.js

--- ui/ProfileCounter.js
+++ ui/ProfileCounter.js
@@ -29,7 +29,7 @@ but.onclick = function(){
     req.send(null);
 };
 
-var submit=document.setElementById("sub_btn");
+var submit=document.getElementById("sub_btn");
 submit.onclick = function(){
 var names=['Reuban','Esther','Prisci','Stephen'];
 var li='';
