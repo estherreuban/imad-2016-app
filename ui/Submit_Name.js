1ca2e0f2e213005028372751d8bdb981841c70b7
estherreuban <estherreuban@gmail.com>
2016-10-20 21:53:24 +0530

    [imad-console] Updates ui/Submit_Name.js

--- ui/Submit_Name.js
+++ ui/Submit_Name.js
@@ -1,1 +1,54 @@
+console.log("Loaded!!!");
+/*var counter=0;
+var but = document.getElementById("button1");
+but.onclick = function(){
+counter++;
+
+var span1=document.getElementById("count1");
+span1.innerHTML=counter.toString();
+};*/
+
+
+var but = document.getElementById("button1");
+
+but.onclick = function(){
+    //////////////Create a request
+    var req = new XMLHttpRequest();
+    
+    req.onreadystatechange = function (){
+        if (req.readyState === XMLHttpRequest.DONE ){
+    
+            if (req.status === 200){
+                var counter = req.responseText;
+                var span1=document.getElementById("count1");
+                span1.innerHTML=counter.toString();
+            }
+        }
+    };
+    req.open('GET', 'http://estherreuban.imad.hasura-app.io/counter', true);
+    req.send(null);
+};
+
+
+
+
+
+
+
+
+var submit=document.getElementById("sub_btn");
+submit.onclick = function(){
+var names=['Reuban','Esther','Prisci','Stephen'];
+var li='';
+for(i=0;i<names.length;i++){
+    li+='<ul>'+names[i]+'</ul>';
+    
+}
+
+var ul=document.getElementById('NameList');
+
+ul.innerHTML =li;
+
+
+}
 
