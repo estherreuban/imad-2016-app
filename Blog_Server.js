76f2b638b70584f0a7f72a17e5df73a2acc368cd
estherreuban <estherreuban@gmail.com>
2016-11-08 22:44:46 +0530

    [imad-console] Updates Blog_Server.js

--- Blog_Server.js
+++ Blog_Server.js
@@ -2,11 +2,9 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+
 var app = express();
 
-console.log("Loaded!!!");
-app.get(`/blog.html`, function(req, res){
+app.get('/blog', function(req, res){
    res.sendFile(path.join(__dirname, 'ui', 'Blog.html'));
 });
-
-
