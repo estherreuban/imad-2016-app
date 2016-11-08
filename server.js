4defb908417dc3cab79ca9768e9baf3617a0404c
estherreuban <estherreuban@gmail.com>
2016-11-08 22:43:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+
+var app = express();
+
 app.get('/blog', function(req, res){
    res.sendFile(path.join(__dirname, 'ui', 'Blog.html'));
 });
-var app = express();
 var Pages= {
         'one':{
         title:'Page 1',
