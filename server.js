4678e9c99697c28a78a04810a0a75e7a3c25044f
estherreuban <estherreuban@gmail.com>
2016-11-08 21:59:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,3 +141,8 @@ var port = 8080; // Use 8080 for local development because you might already hav
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+
+app.get(`/blog.html`, function(req, res){
+   res.sendFile(path.join(__dirname, 'ui', 'Blog.html'));
+});
